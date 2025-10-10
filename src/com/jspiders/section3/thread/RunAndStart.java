34c2be809cb244fdc31f9a164e6ef0a3135ff7c9
x9code <kumarranad672@gmail.com>
2025-10-11 00:00:48 +0530

    2025-10-11

--- src/com/jspiders/section3/thread/RunAndStart.java
+++ src/com/jspiders/section3/thread/RunAndStart.java
@@ -1,15 +1,23 @@
 package com.jspiders.section3.thread;
 
-class MyThread extends Thread {
+class MyThread1 extends Thread {
     public void run() {
         System.out.println("Running in : " + Thread.currentThread().getName()+" Thread");
     }
 }
+class MyThread2 implements Runnable{
+    public void run(){
+        System.out.println("Running in :"+Thread.currentThread().getName()+" Thread");
+    }
+}
 public class RunAndStart {
     public static void main(String[] args) {
-        MyThread t1 = new MyThread();
-        t1.start(); // Creates new thread
-        t1.run();   // Executes in main thread
+        MyThread1 mt1 = new MyThread1();
+        mt1.start(); // Creates new thread
+        mt1.run();   // Executes in main thread
+        MyThread2 mt2 = new MyThread2();
+        Thread t1 = new Thread(mt2);
+        t1.start();
     }
 }
 
