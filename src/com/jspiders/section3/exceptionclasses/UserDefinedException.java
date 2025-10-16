2939ba123b78e5979091a75c9edc0ebaf5aaa8ad
x9code <kumarranad672@gmail.com>
2025-10-16 23:14:36 +0530

    minor change in user defined exception program

--- src/com/jspiders/section3/exceptionclasses/UserDefinedException.java
+++ src/com/jspiders/section3/exceptionclasses/UserDefinedException.java
@@ -3,27 +3,27 @@ package com.jspiders.section3.exceptionclasses;
 import java.util.Scanner;
 
 class IllegalAge extends Exception{
-//    IllegalAge(String message){
-//        super(message);
-//    }
+    IllegalAge(String message){
+        super(message);
+    }
 }
 public class UserDefinedException {
     static void checkAge(int age) throws IllegalAge{
         if(age < 18){
-            System.out.println("Age must be greater than 18");
+           throw new IllegalAge("Age must be greater than 18");
         }else{
             System.out.println("eligible for vote");
         }
     }
 
-    static void main() {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your age :");
+        System.out.println("Enter your age : ");
         int age = sc.nextInt();
         try{
             checkAge(age);
         } catch (IllegalAge e) {
-            //e.getMessage();
+            System.out.println(e.getMessage());
         }
     }
 
