d2f892e5575561511c1bf5b6effe370beb20790b
x9code <kumarranad672@gmail.com>
2025-10-18 00:14:30 +0530

    c1

--- src/com/jspiders/section3/filehandling/ReadTheFile.java
+++ src/com/jspiders/section3/filehandling/ReadTheFile.java
@@ -7,14 +7,22 @@ import java.io.IOException;
 
 public class ReadTheFile {
     static void main() throws IOException {
-        File f1 = new File("D:\\folder1\\sample.txt");
+        File f1 = new File("D:\\Demo-folder\\sample1.txt");
         int size = (int)f1.length();
-        FileReader fr = new FileReader(f1);
+        FileReader fr1 = new FileReader(f1);
         char[] data = new char[size];
-        fr.read(data);
+        fr1.read(data);
         String s = String.valueOf(data);
         System.out.println(s);
-        System.out.println("reading the file");
-
+        System.out.println("reading the file 1");
+        System.out.println("----------------------------");
+        File f2 = new File("D:\\Demo-folder\\sample2.txt");
+        int size1 = (int)f1.length();
+        FileReader fr2 = new FileReader(f2);
+        char[] data1 = new char[size1];
+        fr2.read(data);
+        String s1 = String.valueOf(data1);
+        System.out.println(s1);
+        System.out.println("reading the file 2");
     }
 }
--- src/com/jspiders/section3/filehandling/WriteToFile.java
+++ src/com/jspiders/section3/filehandling/WriteToFile.java
@@ -6,10 +6,15 @@ import java.io.IOException;
 
 public class WriteToFile {
     static void main() throws IOException {
-        File f1 = new File("D:\\folder1\\sample1.txt");
-        FileWriter fw = new FileWriter(f1);
-        fw.write("this file is created by using programs");
-        fw.flush();
-        System.out.println("writing to file");
+        File f1 = new File("D:\\Demo-folder\\sample1.txt");
+        FileWriter fw1 = new FileWriter(f1);
+        fw1.write("this file is created by using programs");
+        fw1.flush();
+        System.out.println("writing to file 1");
+        File f2 = new File("D:\\Demo-folder\\sample2.txt");
+        FileWriter fw2 = new FileWriter(f2);
+        fw2.write("this file is created by using programs");
+        fw2.flush();
+        System.out.println("writing to file 2");
     }
 }
