237d9926e07f8e495ea0bc1bef722ba112799f11
x9code <kumarranad672@gmail.com>
2025-10-17 08:41:27 +0530

    changes

--- src/com/jspiders/section3/exceptionclasses/Marks.java
+++ src/com/jspiders/section3/exceptionclasses/Marks.java
@@ -11,7 +11,7 @@ public class Marks {
         if (marks < 0 || marks > 100) {
             throw new InvalidMarksException("Marks must be between 0 and 100.");
         } else {
-            System.out.println("Valid marks: " + marks);
+            System.out.println("Valid marks: "+ marks);
         }
     }
     public static void main(String[] args) {
