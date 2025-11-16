2ca6fcf3e937742c62a30cbe146ce2378707c6df
x9code <kumarranad672@gmail.com>
2025-11-16 17:59:41 +0530

    pattern11

---
+++ src/com/jspiders/pattern/Pattern11.java
@@ -0,0 +1,23 @@
+package com.jspiders.pattern;
+
+public class Pattern11 {
+    public static void main(String[] args){
+        int n = 5;
+        for (int r = 1; r <= n; r++) {
+            for (int s = 1; s <= n - r; s++) {
+                System.out.print(" ");
+            }
+            for (int c = 1; c <= r; c++) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+
+
+    }
+}
+//            1
+//            12
+//            123
+//            1234
+//            12345
