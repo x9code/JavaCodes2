e514c3ce066aa485b7f98d345a29f957b55dbe2b
x9code <kumarranad672@gmail.com>
2025-11-16 17:56:18 +0530

    A

---
+++ src/com/jspiders/pattern/Pattern8.java
@@ -0,0 +1,21 @@
+package com.jspiders.pattern;
+
+public class Pattern8 {
+    public static void main(String[] args){
+        int n = 5;
+        for (int r = 1; r <= n; r++) {
+            for (int s = 1; s <= n - r; s++) {
+                System.out.print(" ");
+            }
+            for (char c = 'A'; c < 'A' + r; c++) {
+                System.out.print(c);
+            }
+            for (char c = (char) ('A' + r - 2); c >= 'A'; c--) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+
+
+    }
+}
